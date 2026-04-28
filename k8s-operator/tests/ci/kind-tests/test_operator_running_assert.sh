8e746cea411299bf4ebd57c4255748fefaabac98
DarylC <daryl.b.cayaban@accenture.com>
2026-04-28 21:03:07 +0800

    feat: add a test case to check the operator status

---
+++ k8s-operator/tests/ci/kind-tests/test_operator_running_assert.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Purpose: Assert that the gcp-symphony-operator pod is running.
+# Run from k8s-operator/tests/ci/kind-tests with an isolated Kubernetes context.
+
+kubectl wait --for=create pod \
+    -l "app=$IMAGE" \
+    --timeout=60s
+
+if ! kubectl wait --for=condition=Ready pod \
+    -l "app=$IMAGE" \
+    --timeout=60s; then
+    REASON=$(kubectl get pods -l app=$IMAGE -o jsonpath='{.items[0].status.containerStatuses[0].state.waiting.reason}')
+    MESSAGE=$(kubectl get pods -l app=$IMAGE -o jsonpath='{.items[0].status.containerStatuses[0].state.waiting.message}')
+    
+    echo "[FAIL] $IMAGE isn't running properly."
+    echo " - Likely reason: ${REASON:-Unknown} - ${MESSAGE:-No error message provided.}"
+
+    exit 1
+fi
+
+echo "[PASS] $IMAGE is running."
